371fb7495f491b3a87f725f1a58683e4d5ee078c
Sean Perkins <sean@ionic.io>
2022-01-04 15:49:33 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vue-toast-24503',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
